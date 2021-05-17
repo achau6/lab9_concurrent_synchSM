66aa8bf9872b608b1d531874dac0619aeb3dea89
achau6 <52433480+achau6@users.noreply.github.com>
2021-05-16 21:51:44 -0700

    Update achau020_lab9_part1.c

--- turnin/achau020_lab9_part1.c
+++ turnin/achau020_lab9_part1.c
@@ -6,6 +6,7 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *	Demo: https://youtu.be/P7o360zqCr8
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
